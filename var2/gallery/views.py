350de48bfba3ab5b3d5fb78b01f54987aad870b7
xanaxhol1c <loram.hoptan@gmail.com>
2025-05-23 16:10:35 +0300

    created image_detail

--- var2/gallery/views.py
+++ var2/gallery/views.py
@@ -6,5 +6,8 @@ def gallery_view(request):
 
     return render(request, 'gallery.html', {"categories" : categories})
 
+
 def image_detail(request, pk):
-    ...
+    image = get_object_or_404(Image, id=pk)
+
+    return render(request, 'image_detail.html', {"image" : image})
