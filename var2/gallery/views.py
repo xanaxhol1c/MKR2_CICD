d159897ab364b0f0383af47efdc21401fdba5158
xanaxhol1c <loram.hoptan@gmail.com>
2025-05-23 16:10:16 +0300

    created gallery_view

--- var2/gallery/views.py
+++ var2/gallery/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Category, Image
+
+def gallery_view(request):
+    categories = Category.objects.all()
+
+    return render(request, 'gallery.html', {"categories" : categories})
+
+def image_detail(request, pk):
+    ...
